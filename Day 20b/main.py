a13b2ed943f9c093839a68870c4d40aaeaa69bd1
James <jamesmcneice@gmail.com>
2023-12-20 12:06:41 -0700

    more efficient

--- Day 20b/main.py
+++ Day 20b/main.py
@@ -1,8 +1,15 @@
 import re
 import copy
+import queue
+from collections import deque
 
 file_path = 'input.txt'
 workflow_pattern = r'\b(\w+)\{(.*?)\}'
+
+# High is True
+# Low is False
+LOW = False
+HIGH = True
 
 with open(file_path, 'r') as file:
     modules = {}
@@ -20,7 +27,7 @@ with open(file_path, 'r') as file:
             state = {}
             name = name[1:]
         else:
-            type = None
+            type = 'broadcaster'
             state = None
 
         modules[name] = {
@@ -41,26 +48,27 @@ for module_name, module in list(modules.items()):
             }
         modules[destination]['inputs'].append(module_name)
         if modules[destination]['type'] == '&':
-            modules[destination]['state'][module_name] = 'low'
+            modules[destination]['state'][module_name] = LOW
 
 def apply_pulse(source_module_name, pulse, destination_module_name):
     module = modules[destination_module_name]
     next_pulse = None
 
-    if module['type'] is None:
-        next_pulse = pulse
-    elif module['type'] == '%':
-        if pulse == 'low':
+    if module['type'] == '%':
+        if pulse == LOW:
             module['state'] = not module['state']
-            next_pulse = 'high' if module['state'] else 'low'
+            next_pulse = module['state']
     elif module['type'] == '&':
         module['state'][source_module_name] = pulse
-        if all([last == 'high' for last in module['state'].values()]):
-            next_pulse = 'low'
+        if pulse == LOW:
+            # Just got a low, can't be all high, so there's at least one low and we send a High
+            next_pulse = HIGH
         else:
-            next_pulse = 'high'
+            next_pulse = not all([last for last in module['state'].values()])
     elif module['type'] == 'output':
         module['last_received'] = next_pulse
+    elif module['type'] == 'broadcaster':
+        next_pulse = pulse
     else:
         raise TypeError('Unknown module type')
 
@@ -79,7 +87,7 @@ def print_module_state(num_presses):
     overall_state = ""
     for module_name, module in modules.items():
         local_state = ""
-        if module['type'] is None:
+        if module['type'] == 'broadcaster':
             pass
         elif module['type'] == '%':
             if module['state']:
@@ -96,7 +104,16 @@ def print_module_state(num_presses):
         overall_state += local_state
 
     number_of_true = overall_state.count('1')
-    print(f'{num_presses}: {overall_state} : {number_of_true}')
+    print(f'{num_presses}: {overall_state} : {number_of_true} : len {len(overall_state)}')
+
+# def module_layers():
+#     modules_to_open = ['broadcaster']
+#     layers = {
+#         0: ['broadcaster']
+#     }
+#
+#     while modules_to_open:
+#         mo
 
 button_presses = 0
 seen_polarities = {}
@@ -109,15 +126,23 @@ for module in modules.keys():
         'none': []
     }
 
+num_pulses = {
+    HIGH: 0,
+    LOW: 0,
+}
+
 while True:
+#for i in range(1000):
     #print_module_state(button_presses)
     button_presses += 1
-    pulses = [('start', 'low', 'broadcaster')]
+    pulses = deque([('start', LOW, 'broadcaster')], 100)
+    #pulses = [('start', LOW, 'broadcaster')]
     polarities = ""
     while pulses:
-        pulse = pulses.pop(0)
+        pulse = pulses.popleft()
+        #pulse = pulses.pop(0)
         num_pulses[pulse[1]] += 1
-        if pulse[2] == 'rx' and pulse[1] == 'low':
+        if pulse[2] == 'rx' and pulse[1] == LOW:
             print(f'Sent low to rx after {button_presses} button presses')
             exit(1)
         # elif pulse[2] == 'rx':
@@ -125,11 +150,11 @@ while True:
         #     print(f"State of dt {modules['dt']['state']}")
         #     print("")
         new_pulses = apply_pulse(*pulse)
-        seen_sends[pulse[2]]['calls'] += 1
-        if new_pulses:
-            seen_sends[pulse[2]][new_pulses[0][1]].append(seen_sends[pulse[2]]['calls'])
-        else:
-            seen_sends[pulse[2]]['none'].append(seen_sends[pulse[2]]['calls'])
+        #seen_sends[pulse[2]]['calls'] += 1
+        # if new_pulses:
+        #     seen_sends[pulse[2]][new_pulses[0][1]].append(seen_sends[pulse[2]]['calls'])
+        # else:
+        #     seen_sends[pulse[2]]['none'].append(seen_sends[pulse[2]]['calls'])
         # if new_pulses:
         #     polarity = "1" if new_pulses[0][1] == "high" else "0"
         #     #print(f"{pulse} caused {polarity} : {new_pulses}")
@@ -138,6 +163,9 @@ while True:
         #     print(f"{pulse} caused - {new_pulses}")
         pulses.extend(new_pulses)
 
+    if button_presses % 100000 == 0:
+        print(f'Done press {button_presses}')
+
     # if polarities in seen_polarities:
     #     seen_polarities[polarities].append(button_presses)
     # else:
@@ -145,6 +173,6 @@ while True:
     #     print(f"New polarities {button_presses}: {polarities}")
     #     polarity = 'just here to debug'
 
-#print(f"Done after {num_pulses}, {num_pulses['low'] * num_pulses['high']}")
+print(f"Done after {num_pulses}, {num_pulses[LOW] * num_pulses[HIGH]}")
 
 
