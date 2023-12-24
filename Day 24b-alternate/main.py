cee28ad829129a933d8a55da57243143624b56c1
James <jamesmcneice@gmail.com>
2023-12-24 16:33:18 -0700

    Working alternate version. Takes awhile to brute force the velocities though

--- Day 24b-alternate/main.py
+++ Day 24b-alternate/main.py
@@ -40,7 +40,7 @@ with open(file_path, 'r') as file:
         velocity = list([int(i) for i in velocity.split(',')])
         stones.append((coord, velocity))
 
-def apply_direction(coords, direction, ticks=200000000):
+def apply_direction(coords, direction, ticks=1000000000000):
     next_coord = (coords[0]+direction[0]*ticks, coords[1]+direction[1]*ticks, coords[2]+direction[2]*ticks)
     return next_coord
 
@@ -51,14 +51,15 @@ def line_segment(coord, velocity):
 def subtract_coord(a, b):
     return (a[0]-b[0], a[1]-b[1], a[2]-b[2])
 
-v_max = 100+ 1
-v_min = -100
+v_max = 300+1
+v_min = -10
 
+tolerance = 0.01
 def compare_points(a,b):
     # Had rounding problems with shapely
-    if int(a.x) == int(b.x):
-        if int(a.y) == int(b.y):
-            if int(a.z) == int(b.z):
+    if abs(a.x - b.x) < tolerance:
+        if abs(a.y - b.y) < tolerance:
+            if abs(a.z - b.z) < tolerance:
                 return True
     return False
 
@@ -69,15 +70,15 @@ for xd in range(v_min, v_max):
             # if xd == -3 and yd == 1 and zd == 2:
             #     print('Test correct')
             thrown_velocity = (xd,yd,zd)
+            #thrown_velocity = (263, 120, 21)
             test_stones_in_thrown_stone_reference_frame = list([(coord, subtract_coord(v, thrown_velocity)) for coord, v in test_stones])
+
             test_line_segments = list([line_segment(coord, velocity) for coord, velocity in test_stones_in_thrown_stone_reference_frame])
             first_intersection = test_line_segments[0].intersection(test_line_segments[1])
             if type(first_intersection) is Point:
                 second_intersection = test_line_segments[1].intersection(test_line_segments[2])
                 if type(second_intersection) is Point:
                     if compare_points(first_intersection, second_intersection):
-                        print(f'Starting point was: ({first_intersection.x}, {first_intersection.y}, {first_intersection.z}) with velocity {thrown_velocity}')
-                        print(f"Sum is {int(first_intersection.x) + int(first_intersection.y) + int(first_intersection.z)}")
-                        exit()
+                        print(f'Possibly point was: ({first_intersection.x}, {first_intersection.y}, {first_intersection.z}) with velocity {thrown_velocity}. Sum is {int(first_intersection.x) + int(first_intersection.y) + int(first_intersection.z)}')
 
 print(f'Took {time.time() - start_time} seconds')
